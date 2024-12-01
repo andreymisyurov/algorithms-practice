af42a00097b0256c9a9473a1ea0f37216b80cb26
Andrey Misyurov <andreymisyurov@Andreys-MacBook-Air.local>
2024-12-01 11:32:55 +0500

    Add merge k lists(demo)

--- linked_list/algo.cpp
+++ linked_list/algo.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 struct ListNode {
     int val;
@@ -22,11 +23,15 @@ public:
     LinkedList() : m_head(nullptr) {}
 
     ~LinkedList() {
-        for(;m_head;) {
-            struct ListNode* tmp = m_head;
-            m_head = m_head->next;
-            delete tmp;
-        }
+        // for(;m_head;) {
+        //     struct ListNode* tmp = m_head;
+        //     m_head = m_head->next;
+        //     delete tmp;
+        // }
+    }
+
+    ListNode* get_head() {
+        return m_head;
     }
 
     void add_node(int in_value) {
@@ -140,27 +145,106 @@ public:
     bool palindrom() {
         ListNode* head = m_head;
         ListNode* tail = reverse(middle_node());
+        ListNode* tail_copy = tail;
         for(;head && tail;) {
             if (head->val != tail->val)
                 return false;
             head = head->next;
             tail = tail->next;
         }
+        reverse(tail_copy);
         return true;
     }
+
+// 1 2 3 4 5 6
+    void reorder() {
+        ListNode* middle = middle_node();
+        ListNode* tail = reverse(middle->next);
+        ListNode* head = m_head;
+        middle->next = nullptr;
+        for(;tail;) {
+            ListNode* temp1 = head->next;
+            ListNode* temp2 = tail->next;
+
+            head->next = tail;
+            tail->next = temp1;
+
+            head = temp1;
+            tail = temp2;
+        }
+    }
+
+    static ListNode* merge_two_lists(ListNode* first, ListNode* second) {
+        ListNode dummy(0);
+        ListNode* tmp = &dummy;
+        for(;first && second;) {
+            if(first->val <= second->val) {
+                tmp->next = first;
+                first = first->next;
+            } else {
+                tmp->next = second;
+                second = second->next;
+            }
+            tmp = tmp->next;
+        }
+        tmp->next = first ? first : second;
+        return dummy.next;
+    }
+
+    static ListNode* merge_k_lists(std::vector<ListNode*> data) {
+        ListNode dummy(0);
+        ListNode* tmp = &dummy;
+        int d = 15;
+        while(d) {
+            int min = INT32_MAX;
+            int min_i = -1;
+            for (int i = 0; i < data.size(); ++i) {
+                if (data[i] != nullptr && data[i]->val < min) {
+                    min = data[i]->val;
+                    min_i = i;
+                }
+            }
+            if (data[min_i]) {
+                tmp->next = data[min_i];
+                tmp = tmp->next;
+                data[min_i] = data[min_i]->next;
+            }
+            --d;
+        }
+        return dummy.next;
+    }
+
 };
 
 int main() {
-    LinkedList list;
-    list.add_node(1);
-    list.add_node(2);
-    list.add_node(3);
-    list.add_node(4);
-    list.add_node(3);
-    list.add_node(2);
-    list.add_node(1);
-    std::cout << list.palindrom();
-    // list.print();
+    LinkedList list1;
+    list1.add_node(1);
+    list1.add_node(1);
+    list1.add_node(3);
+    list1.add_node(6);
+    list1.add_node(6);
+    list1.add_node(7);
+    list1.add_node(10);
+
+    LinkedList list2;
+    list2.add_node(5);
+    list2.add_node(5);
+    list2.add_node(8);
+    list2.add_node(99);
+
+
+    LinkedList list3;
+    list3.add_node(1);
+    list3.add_node(2);
+    list3.add_node(4);
+    list3.add_node(9);
+
+    std::vector<ListNode*> vec;
+    vec.push_back(list1.get_head());
+    vec.push_back(list2.get_head());
+    vec.push_back(list3.get_head());
+
+    LinkedList::print(LinkedList::merge_k_lists(vec));
     // list.print(list.reverse(list.middle_node()));
     // list.removeNodeFromEnd((size_t)7);
     // list.reverse();
